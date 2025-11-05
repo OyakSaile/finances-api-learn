e08301b6159ed7771ec0445412acd27bc9c1cb27
OyakSaile <contatodevkayo@gmail.com>
2025-11-04 23:08:37 -0300

    refactor: streamline app configuration and update status response

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,15 @@
 import 'dotenv/config'
 import Fastify from 'fastify'
-
-const PORT = Number(process.env.PORT)
-
-if (!PORT) {
-    throw new Error('PORT NOT DEFINED.')
-}
+import { env } from './config/env'
 
 const fastify = Fastify({
     logger: true,
 })
 
 fastify.get('/status', (_, response) => {
-    return response.status(200).send({
-        message: 'Working',
-    })
+    response.status(200).send({ message: 'Server OK' })
 })
 
-fastify.get('/', (_, response) => {
-    return response.status(200).send({
-        message: 'Working',
-    })
-})
-
-fastify.listen({ port: PORT }, () => {
-    console.log(`port listening at ${PORT}`)
+fastify.listen({ port: env?.PORT }, () => {
+    console.log(`port listening at ${env?.PORT}`)
 })
